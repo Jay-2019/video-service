e00dc46940d3a2d6d6d2156847239d801e2b0dd3
Jay Prakash Maurya <jayprakash.maurya003@gmail.com>
2025-01-06 06:22:51 +0530

    git commit -m "docs(swagger): add API documentation
    - Add Swagger UI integration
    - Configure API endpoints documentation
    - Document all API endpoints with examples"

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const { sequelize } = require('./src/models');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 require('./src/database/database');
 require('./src/redis/connection');
 
 const videoRoutes = require('./src/routes/video.routes');
-const errorHandler = require('./src/utils/errorHandler');
+const handleError = require('./src/utils/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,21 +18,33 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/ping', (req, res) => {
-    res.json({ message: 'pong' });
+app.use('/api', (req, res, next) => {
+    if (req.path === '/ping') {
+        return res.json({ message: 'pong' });
+    }
+    next();
 });
+
 app.use('/api/videos', videoRoutes);
 
-app.use(errorHandler);
+// Swagger setup
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    if (!res.headersSent) {
+        handleError(res, err);
+    }
+});
 
 sequelize.sync({ force: true })
-  .then(() => {
-    console.log('Database synchronized');
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://127.0.0.1:${PORT}`);
+    .then(() => {
+        console.log('Database synchronized');
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://127.0.0.1:${PORT}`);
+            console.log(`Swagger docs available at http://127.0.0.1:${PORT}/api-docs`);
+        });
+    })
+    .catch(err => {
+        console.error('Unable to synchronize the database:', err);
     });
-  })
-  .catch(err => {
-    console.error('Unable to synchronize the database:', err);
-  });
